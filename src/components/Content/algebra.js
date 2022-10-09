3be27c17586f416c7e3893ddcf8ab4e22b312e44
Vaishnavi879 <vaishnavikb.cs20@rvce.edu.in>
2022-10-09 17:00:46 +0530

    my changes

--- src/components/Content/algebra.js
+++ src/components/Content/algebra.js
@@ -6,12 +6,12 @@ function Algebra() {
             <h1 style={{padding: "40px"}} className="home">Welcome to Algebra Helper!</h1>
             <div className="video-container">
                 <iframe title = "video1" src="https://www.youtube.com/embed/szovCVjZK_k" className="video-layout"></iframe>
-                <iframe title = "video1" src="https://www.youtube.com/embed/szovCVjZK_k" className="video-layout"></iframe>
-                <iframe title = "video1" src="https://www.youtube.com/embed/szovCVjZK_k" className="video-layout"></iframe>
-                <iframe title = "video1" src="https://www.youtube.com/embed/szovCVjZK_k" className="video-layout"></iframe>
-                <iframe title = "video1" src="https://www.youtube.com/embed/szovCVjZK_k" className="video-layout"></iframe>
-                <iframe title = "video1" src="https://www.youtube.com/embed/szovCVjZK_k" className="video-layout"></iframe>
-                <iframe title = "video1" src="https://www.youtube.com/embed/szovCVjZK_k" className="video-layout"></iframe>
+                <iframe title = "video1" src="https://www.youtube.com/embed/r6Qqmy2SYko" className="video-layout"></iframe>
+                <iframe title = "video1" src="https://www.youtube.com/embed/8kvcCoRuJcI" className="video-layout"></iframe>
+                <iframe title = "video1" src="https://www.youtube.com/embed/QY9Li9WTcEk" className="video-layout"></iframe>
+                <iframe title = "video1" src="https://www.youtube.com/embed/U-VUAvewqeI" className="video-layout"></iframe>
+                <iframe title = "video1" src="https://www.youtube.com/embed/TkM5dK0AHUM" className="video-layout"></iframe>
+                <iframe title = "video1" src="https://www.youtube.com/embed/C6jYp1WsnmU" className="video-layout"></iframe>
             </div>
         </div>
     );
